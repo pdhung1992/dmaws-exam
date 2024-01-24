476c18835578f79b427983a2e00b77a60f1b4592
duyhungphung <phungduyhung1992@gmail.com>
2024-01-24 10:08:59 +0700

    exam

--- order-api/Models/EditOrderModel.cs
+++ order-api/Models/EditOrderModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace order_api.Models;
 
 public class EditOrderModel
 {
+    [Required(ErrorMessage = "Order delivery is required")]
     public string OrderDelivery { get; set; }
     
+    [Required(ErrorMessage = "Order Address is required")]
     public string OrderAddress { get; set; }
 }
